3d962f4de984b8a25427b5d93fa99e8caf4752ec
muhammadahmadmslm <muhammad.ahmad@mslm.io>
2023-07-17 23:10:57 +0500

    test

--- src/ncd_nft/build.sh
+++ src/ncd_nft/build.sh
@@ -13,6 +13,8 @@ cd "$(dirname "$0")"
 cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
 cd "$(dirname "$0")"
 cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+
+
 cd "$(dirname "$0")"
 cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
 cd "$(dirname "$0")"
