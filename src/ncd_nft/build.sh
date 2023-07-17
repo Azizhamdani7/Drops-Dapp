b74dea45030b095921e426c10e786bc0c7d7f1f3
muhammadahmadmslm <muhammad.ahmad@mslm.io>
2023-07-17 22:59:56 +0500

    sh

--- src/ncd_nft/build.sh
+++ src/ncd_nft/build.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
+cargo build --all --target wasm32-unknown-unknown --release#!/bin/bash
+cd "$(dirname "$0")"
 cargo build --all --target wasm32-unknown-unknown --release
